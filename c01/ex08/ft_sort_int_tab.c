40ee2f10c151cce64a935eec4e52e8022ffc9df7
Bryan <89350478+sabahbryan@users.noreply.github.com>
2024-01-29 22:27:16 +0800

    Add files via upload

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: bryaloo <bryaloo@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 13:36:07 by bryaloo           #+#    #+#             */
-/*   Updated: 2024/01/27 18:08:26 by bryaloo          ###   ########.fr       */
+/*   Updated: 2024/01/29 19:07:50 by bryaloo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 void	swap(int *a, int *b)
 {
-	int	tmp = *a;
+	int	tmp;
+
+	tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
@@ -41,5 +43,18 @@ void	ft_sort_int_tab(int *tab, int size)
 
 int	main(void)
 {
+	int	tab[9] = {25, 7, 4, 5, 19, 68, 12, 42, 88};
+	int	size = 9;
+	int	i;
 
+	ft_sort_int_tab(tab, size);
+	i = 0;
+	printf("Sorted Array: ");
+	while (i < size)
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	printf("\n");
+	return (0);
 }
